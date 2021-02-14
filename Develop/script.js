88bd5833e766a07cc129f2c4a1becf6289d31488
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-02-14 11:57:45 -0600

    Fixed passwordSelection for reselecting pass param

--- Develop/script.js
+++ Develop/script.js
@@ -76,17 +76,8 @@ function getRandomValue(min, max){
   var random = Math.floor(Math.random()*(max-min+1)+min);
   return random;
 }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
